692c4d4f64b9272c53c00804c4549bb26e0b3ea9
NNogue <noguebn@gmail.com>
2018-04-24 02:48:22 +0200

    json

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
 var server_port = process.env.8080 || process.env.PORT || 80;
 var server_host = process.env.192 || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+server.listen(process.env.PORT, '0.0.0.0')
